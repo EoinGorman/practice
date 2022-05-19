cad268ff99991b91bed6ae865d5f079e1eb3e27f
EoinGorman <eoingorman@live.ie>
2022-05-19 22:13:24 +0100

    D of CRUD and tidy up sample a bit

--- ruby/postgres/app.rb
+++ ruby/postgres/app.rb
@@ -14,16 +14,13 @@ class App
     @conn.close if @conn
   end
 
-  def create_user
+  def create_user(username, password)
     puts "Creating User."
-    puts "Enter username: "
-    username = STDIN.gets.chomp
-    puts "Enter password: "
-    password = STDIN.gets.chomp
     @conn.exec("INSERT INTO users (username, password, created_at) VALUES('#{username}', '#{password}', NOW())")
   end
 
   def display_users
+    puts "Users:"
     result = @conn.exec('SELECT * FROM users')
     result.each do |res|
       puts "#{res.class} : #{res}"
@@ -31,17 +28,39 @@ class App
   end
 
   def update_user_password(user, new_password)
+    puts "Updating user password for #{user}..."
     @conn.exec("UPDATE users SET password = '#{new_password}' WHERE username = '#{user}'")
+  end
+
+  def delete_user(user)
+    puts "Deleting user #{user}..."
+    @conn.exec("DELETE FROM users WHERE username = '#{user}'")
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   begin
+    new_user = "Bond"
+    new_password = "JamesBond"
+
     app = App.new
     app.connect
+
+    # R
     app.display_users
-    #app.create_user
-    app.update_user_password('Bond', 'newpass')
+
+    # C
+    app.create_user(new_user, new_password)
+    app.display_users
+
+    # U
+    new_password = 'newpass'
+    app.update_user_password(new_user, new_password)
+    app.display_users
+
+    # D
+    app.delete_user(new_user)
+    app.display_users
   rescue PG::Error => e
     puts "#{e.message}"
   ensure
